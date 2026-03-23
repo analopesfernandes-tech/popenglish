c676d56c6ea46d4d29af48b158628d971e22a729
AnaFernandes <analopesfernandesmai@gmail.com>
2026-03-23 10:21:19 +0100

    fix port log

--- popenglish-backend/server.js
+++ popenglish-backend/server.js
@@ -85,7 +85,9 @@ app.post("/users/login", (req, res) => {
 SERVER
 ========================= */
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta", PORT);
 });
 
