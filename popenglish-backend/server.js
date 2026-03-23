2321b99ebf27f52ef5e48eb184b769783c585666
AnaFernandes <analopesfernandesmai@gmail.com>
2026-03-23 09:46:13 +0100

    fix: conexão com DATABASE_URL

--- popenglish-backend/server.js
+++ popenglish-backend/server.js
@@ -9,12 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect(err => {
     if(err){
